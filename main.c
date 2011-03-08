98e718bfbaff0de58a56bb518836965ddc2cf93a
David Caldwell <david@porkrind.org>
2011-03-08 11:50:21 -0800

    [main.c] Add command line arg to send different apple event commands.

--- main.c
+++ main.c
@@ -1,11 +1,29 @@
 #include "RestartShutdownLogout.h"
 #include <stdio.h>
 
-int main(void)
+void usage(char *me)
 {
-    OSStatus status = SendAppleEventToSystemProcess(kAESleep);
+    printf("Usage:\n"
+           "\t%s shutdown|restart|sleep|logout\n", me);
+    exit(EXIT_FAILURE);
+}
+
+int main(int c, char **v)
+{
+    char *command = NULL;
+    if (c == 2) command = v[1];
+    if (!command) usage(v[0]);
+
+    AEEventID event = 0;
+    if (strcmp(command, "shutdown") == 0) event = kAEShutDown;
+    if (strcmp(command, "restart")  == 0) event = kAERestart;
+    if (strcmp(command, "sleep")    == 0) event = kAESleep;
+    if (strcmp(command, "logout")   == 0) event = kAEReallyLogOut;
+    if (event == 0) usage(v[0]);
+
+    OSStatus status = SendAppleEventToSystemProcess(event);
     if (status != noErr)
-        printf("Computer wouldn't sleep: %d\n", (int)status);
+        printf("Computer wouldn't %s: %d\n", command, (int)status);
 
     return status == noErr ? EXIT_SUCCESS : EXIT_FAILURE;
 }
