3320fb5aca6d7bfd0d31694d47e6c830c911f728
David Caldwell <david@porkrind.org>
2011-03-08 11:27:22 -0800

    [main.c] Cleanup.

--- main.c
+++ main.c
@@ -1,18 +1,10 @@
 #include "RestartShutdownLogout.h"
 #include <stdio.h>
 
-/*****************************************************
- * Main
- *****************************************************
- * Purpose:  Demonstrating the SendEventToSystem functions.
- *****************************************************/
 int main(void)
 {
-    OSStatus error = noErr;
-    
-    //sending sleep event to system
-    error = SendAppleEventToSystemProcess(kAESleep);
-    if (error != noErr)
+    OSStatus status = SendAppleEventToSystemProcess(kAESleep);
+    if (status != noErr)
         printf("Computer wouldn't sleep: %d\n", status);
 
     return status == noErr;
